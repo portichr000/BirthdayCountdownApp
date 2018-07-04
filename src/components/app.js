afbca289d3e5ba724792464fbc238c865b656e0e
Christian <chris6por@gmail.com>
2018-07-04 15:27:24 +0000

    fixed some styles

--- src/components/app.js
+++ src/components/app.js
@@ -37,7 +37,6 @@ import LargeText from './largeText';
           <h1 className="grid__title">Birthday Countdown</h1>
       
       
-          <div className="grid__skew-dark-one-box"></div>
           <div className="grid__skew-dark-two"></div>
           <div className="grid__skew-dark-three"></div>
           
